f0ba6f89acfb8a9f2bd4779b51b5b63d04e7e0df
Vincenzo <vincedinardo21@gmail.com>
2022-05-05 17:13:52 +0200

    Fix griglia

--- js/myscript.js
+++ js/myscript.js
@@ -4,19 +4,20 @@ const numero = document.querySelector("ul.my-number");
 // Stampo i numeri da 1-100
 for(let i = 1; i < 101; i++){
     const elemento = document.createElement("li");
-    const element = "Multiplo di " + i;
+ 
+    
 
     if(i % 3 === 0 && i % 5 != 0) {
         console.log(i + " multiplo di 3");
-        elemento.append(i + " Multiplo di 3");
+        elemento.append("Fizz");
         numero.append(elemento);
     } else if(i % 5 === 0 && i % 3 != 0) {
-        console.log(i + " multiplo di 5");
-        elemento.append(i + " Multiplo di 5");
+        console.log("Buzz");
+        elemento.append("Buzz");
         numero.append(elemento);
     } else if(i % 5 === 0 && i % 3 === 0) {
-        console.log(i + " FizzBuzz");
-        elemento.append(i + " FizzBuzz");
+        console.log("FizzBuzz");
+        elemento.append("FizzBuzz");
         numero.append(elemento);
     } else {
         console.log(i);
